92656ec2db9c5a1f71983abca80dcb33f1338544
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-09-08 12:40:21 -0300

    Projeto Criado

---
+++ Exercicios/Variáveis e Expressões/exe1.rb
@@ -0,0 +1,1 @@
+print "digite um número: ";
---
+++ teste.rb
@@ -0,0 +1,32 @@
+
+
+#11.times{|i| puts "#{i}"};
+#1.upto(10) {|x| puts x};
+
+a = [2,3,1]
+a[3] = a[2] -1
+
+a.each do |elt| 
+  print elt+1
+end 
+
+b = [1,2,3,4]
+c = b.map {|x|  puts x*x}
+
+def squre(x)
+  x*x
+end
+
+puts squre(9);
+
+def Math.square(x)
+  x*x
+end
+
+puts Math.square(16);
+
+=begin 
+
+  Aqui é um comentário de Bloco
+
+=end
