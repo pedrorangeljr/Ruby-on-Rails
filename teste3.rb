5c478675086f81526e530c999b658924799791c4
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-09-26 15:19:46 -0300

    strings e vetores(arrays)

---
+++ teste3.rb
@@ -0,0 +1,17 @@
+array = [1,2,3,4,5]
+for element in array 
+  puts element
+end
+
+hash = {:a=>1, :b=>2, :c=>3}
+for key, value in hash
+  puts "#{key} => #{value}"
+end
+
+puts "--------------------------------"
+
+hash.each do |key, value|
+
+  puts "#{key} => #{value}"
+  
+end
